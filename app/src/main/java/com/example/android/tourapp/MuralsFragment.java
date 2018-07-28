b59356f859bdbd421cf278a69f6a515891d71769
paintedlbird7 <rosaperezlarios@gmail.com>
2018-07-27 19:25:43 -0700

    Tour app

--- app/src/main/java/com/example/android/tourapp/MuralsFragment.java
+++ app/src/main/java/com/example/android/tourapp/MuralsFragment.java
@@ -20,51 +20,51 @@ import java.util.ArrayList;
 public class MuralsFragment extends Fragment {
 //
 //    /** Handles playback of all the sound files */
-//    private MediaPlayer mMediaPlayer;
+    private MediaPlayer mMediaPlayer;
 //
 //    /** Handles audio focus when playing a sound file */
-//    private AudioManager mAudioManager;
+    private AudioManager mAudioManager;
 //
 //    /**
 //     * This listener gets triggered whenever the audio focus changes
 //     * (i.e., we gain or lose audio focus because of another app or device).
 //     */
-//    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-//        @Override
-//        public void onAudioFocusChange(int focusChange) {
-//            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-//                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-//                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
-//                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
-//                // our app is allowed to continue playing sound but at a lower volume. We'll treat
-//                // both cases the same way because our app is playing short sound files.
-//
-//                // Pause playback and reset player to the start of the file. That way, we can
-//                // play the word from the beginning when we resume playback.
-//                mMediaPlayer.pause();
-//                mMediaPlayer.seekTo(0);
-//            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-//                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
-//                mMediaPlayer.start();
-//            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-//                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
-//                // Stop playback and clean up resources
-//                releaseMediaPlayer();
-//            }
-//        }
-//    };
-//
-//    /**
-//     * This listener gets triggered when the {@link MediaPlayer} has completed
-//     * playing the audio file.
-//     */
-//    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-//        @Override
-//        public void onCompletion(MediaPlayer mediaPlayer) {
-//            // Now that the sound file has finished playing, release the media player resources.
-//            releaseMediaPlayer();
-//        }
-//    };
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
 
     public MuralsFragment() {
         // Required empty public constructor
@@ -76,13 +76,11 @@ public class MuralsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.word_list, container, false);
 
 //        // Create and setup the {@link AudioManager} to request audio focus
-//        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
 
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-
-
-        words.add(new Word("Lila Gemelos", "Heart of Silicon Valley", R.drawable.eastridge));
+        words.add(new Word("Lila Gemelos", "Heart of Silicon Valley", R.drawable.eastridge, R.raw.number_three));
         words.add(new Word("Carlos Rodriguez", "Alum Rock", R.drawable.alum_rock, R.raw.number_three));
         words.add(new Word("Jose Meza V", "Mural de La Raza", R.drawable.mural_de_la_raza, R.raw.number_five));
 
@@ -106,7 +104,7 @@ public class MuralsFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 // Release the media player if it currently exists because we are about to
                 // play a different sound file
-                //releaseMediaPlayer();
+                releaseMediaPlayer();
 
                 // Get the {@link Word} object at the given position the user clicked on
                 Word word = words.get(position);
@@ -114,56 +112,56 @@ public class MuralsFragment extends Fragment {
                 // Request audio focus so in order to play the audio file. The app needs to play a
                 // short audio file, so we will request audio focus with a short amount of time
                 // with AUDIOFOCUS_GAIN_TRANSIENT.
-//                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-//                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-//
-//                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-//                    // We have audio focus now.
-//
-//                    // Create and setup the {@link MediaPlayer} for the audio resource associated
-//                    // with the current word
-//                    mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
-//
-//                    // Start the audio file
-//                    mMediaPlayer.start();
-//
-//                    // Setup a listener on the media player, so that we can stop and release the
-//                    // media player once the sound has finished playing.
-//                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-//                }
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have audio focus now.
+
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
+                    mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
+
+                    // Start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
 
         return rootView;
     }
 
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//
-//        // When the activity is stopped, release the media player resources because we won't
-//        // be playing any more sounds.
-//        releaseMediaPlayer();
-//    }
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
 
     /**
      * Clean up the media player by releasing its resources.
      */
-//    private void releaseMediaPlayer() {
-//        // If the media player is not null, then it may be currently playing a sound.
-//        if (mMediaPlayer != null) {
-//            // Regardless of the current state of the media player, release its resources
-//            // because we no longer need it.
-//            mMediaPlayer.release();
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
 
             // Set the media player back to null. For our code, we've decided that
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
-//            mMediaPlayer = null;
+            mMediaPlayer = null;
 //
 //            // Regardless of whether or not we were granted audio focus, abandon it. This also
 //            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
-//            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-//        }
-//    }
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
 }
